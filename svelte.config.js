cf646663a88bf5f91d291fcab174c1b607ec3a9d
Yuto Mori <tmori9banner@gmail.com>
2022-03-19 18:24:23 +0900

    ✨ install windicss

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-auto";
+import WindiCSS from "vite-plugin-windicss";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ["PATCH", "DELETE"],
+    },
+    vite: {
+      plugins: [WindiCSS()],
+    },
+  },
 };
 
 export default config;
