9ab354968e3d6db050cadb3b0ccd6aa9eb6d8795
Yuto Mori <tmori9banner@gmail.com>
2022-03-28 19:06:18 +0900

    ✨ レイアウトと内容追加

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,95 @@
-<div class="container mx-auto px-5 py-5">
-	<div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-		<h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">About me</h1>
-		<img class="mt-6 rounded-lg shadow-xl" src="../static/svelte-welcome.png" alt="todo" />
-		<p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Yuto Mori</p>
+<div class="max-w-2xl mx-auto p-2 bg-white" />
+<div class="max-w-2xl mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+	<div class="flex-shrink-0">
+		<img class="h-40 w-40" src="../static/icon.png" alt="icon" />
 	</div>
-	<div class="flex flex-wrap -m-4">
-		<div class="xl:w-1/3 md:w-1/2 p-4">
-			<div class="border border-gray-200 p-6 rounded-lg">
-				<h2 class="text-lg text-gray-900 font-medium title-font mb-2">Work1</h2>
-				<p class="leading-relaxed text-base">description1</p>
-			</div>
-		</div>
-		<div class="xl:w-1/3 md:w-1/2 p-4">
-			<div class="border border-gray-200 p-6 rounded-lg">
-				<h2 class="text-lg text-gray-900 font-medium title-font mb-2">Work2</h2>
-				<p class="leading-relaxed text-base">description2</p>
-			</div>
-		</div>
+	<div class="ml-6 pt-1">
+		<p class="text-3xl row-span-1 col-span-2">Mori Yuto</p>
+		<p class="row-span-1 col-span-2">大学院生 (博士後期課程)</p>
 	</div>
+</div>
+
+<div class="max-w-2xl mx-auto p-6 m-3 bg-white rounded-lg shadow-xl">
+	<div class="text-2xl font-medium text-black mb-4">略歴</div>
+	<ul class="list-disc px-5">
+		<li class="py-0.5">札幌旭丘高等学校 卒業</li>
+		<li class="py-0.5">公立はこだて未来大学複雑系知能学科 卒業</li>
+		<li class="py-0.5">北海道大学大学院情報科学院 修士課程 修了</li>
+		<li class="py-0.5">北海道大学大学院情報科学院 博士後期課程</li>
+	</ul>
+</div>
+
+<div class="max-w-2xl mx-auto p-6 m-3 bg-white rounded-lg shadow-xl text-justify">
+	<div class="text-2xl font-medium text-black mb-4">技術 (wip)</div>
+	<ul class="list-disc px-5">
+		<li>Python, C++, Javascript, Svelte, Tailwindcss, Firebase, Github Actions, 3DCAD,</li>
+	</ul>
+</div>
+
+<div class="max-w-2xl mx-auto p-6 m-3 bg-white rounded-lg shadow-xl">
+	<div class="text-2xl font-medium text-black mb-4">研究</div>
+	<div class="text-lg font-medium text-black mb-2">論文誌 (journal)</div>
+	<ul class="list-none">
+		<li class="py-1">
+			[1] <span class="underline">○Yuto Mori</span>, Soichiro Yokoyama, Tomohisa Yamashita, Hidenori
+			Kawamura, Masato Mori, Norio Kato, Development of an autonomous forearm-supported walker for
+			nursing facilities, Artificial Life and Robotics, Vol. 26, No. 4, pp. 432-441, 2021.
+			(Peer-reviewed)
+		</li>
+	</ul>
+	<div class="text-lg font-medium text-black py-2">学会・研究会</div>
+	<ul class="list-none">
+		<li class="py-1">
+			[2] <span class="underline">○森 雄斗</span>, 横山 想一郎, 山下 倫央, 川村 秀憲, 森 正人,
+			歩行支援機能を有する前腕支持型四輪歩行器の衝突防止機能の開発, 第206回知能システム研究発表会,
+			ICS8, ハイブリッド開催(オンライン/虻田郡留寿都村), 2022.
+		</li>
+		<li class="py-1">
+			[3] <span class="underline">○Yuto Mori</span>, Soichiro Yokoyama, Tomohisa Yamashita, Hidenori
+			Kawamura, Masato Mori, Norio Kato, Development of a forearm-supported walker with walking
+			support and autonomous driving functions, 26th International Symposium on Artificial Life and
+			Robotics (AROB 26th 2021), GS7-4, Online, 2020. (Peer-reviewed)
+		</li>
+		<li class="py-1">
+			[4] <span class="underline">○森 雄斗</span>, 横山 想一郎, 山下 倫央, 川村 秀憲, 森正人,
+			加藤士雄, 前腕支持型四輪歩行器の歩行支援機能の開発, 情報処理北海道シンポジウム2020, 18, 2020.
+			[受賞]
+		</li>
+		<li class="py-1">
+			[5] <span class="underline">○森 雄斗</span>, 鈴木 恵二, 和田 雅昭,
+			定置網内音響画像からの魚種判別・漁獲量予測における汎化性能向上,
+			社会システムと情報技術研究ウィーク, SIG-AI, 2020.
+		</li>
+		<li class="py-1">
+			[6] <span class="underline">○森 雄斗</span>, 鈴木恵二, 和田雅昭,
+			定置網内音響画像からの魚種判別における汎化性能問題, 第19回複雑系マイクロシンポジウム, 2020.
+		</li>
+	</ul>
+	<div class="text-lg font-medium text-black py-2">受賞</div>
+	<ul class="list-disc px-5">
+		<li class="py-1">自動制御学会(SICE)優秀学生賞, 北海道大学大学院 修士論文, 2022.</li>
+		<li class="py-1">技術研究賞, 情報処理北海道シンポジウム2020, 2020.</li>
+	</ul>
+</div>
+
+<div class="max-w-2xl mx-auto p-6 m-3 bg-white rounded-lg shadow-xl">
+	<div class="text-2xl font-medium text-black mb-4">成果物</div>
+	<ul class="list-disc px-5">
+		<li class="py-0.5">
+			2018年度プロジェクト学習 ビーコンIoTで函館のまちをハックする – BEACON FUN Reloaded
+			「＠ハナセル」
+		</li>
+		<li class="py-0.5">Alexa スキル 「ゆで卵タイマー」(現在停止中)</li>
+		<li>LINE Clova スキル「ナンバーパンチ ~数字推測×ボクシング~」</li>
+	</ul>
+</div>
+
+<div class="max-w-2xl mx-auto p-6 m-3 bg-white rounded-lg shadow-xl text-justify">
+	<div class="text-2xl font-medium text-black mb-4">資格，実績</div>
+	<ul class="list-disc px-5">
+		<li>基本情報技術者試験 合格</li>
+		<li>G検定 (JDLA Deep Learning for GENERAL 2019 #3) 合格</li>
+		<li>enPiT-BizSysD ビジネスシステムデザイン分野 (公立はこだて未来大学) 修了</li>
+		<li>enPiT-AiBiC ビッグデータ・AI分野 (東京工業大学) 修了了</li>
+	</ul>
 </div>
